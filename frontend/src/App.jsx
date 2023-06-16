92585b691aad98ae49da6a4beb53e817f0c7d337
bisraty <bisrat2007manu.com>
2023-06-16 14:39:29 +0300

    added assets and packages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,6 @@
 
+import Loginpage from "./auth/Loginpage";
+import Signuppage from "./auth/Signuppage";
 import Createpage from "./screen/Createpage";
 import Homepage from "./screen/Homepage";
 import {Route, Routes} from 'react-router-dom'
@@ -10,6 +12,9 @@ function App() {
   <Routes>
         <Route path='/' element={<Homepage />} />
         <Route path='/create' element={<Createpage />} />
+        <Route path='/login' element={<Loginpage />} />
+        <Route path='/signup' element={<Signuppage />} />
+
         <Route path='*' element={<Homepage />} />
       </Routes>
   )
