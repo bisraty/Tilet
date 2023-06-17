c62c80f7cb1fb5187e4f683fbeeb00dbbde19c03
bisraty <bisrat2007manu.com>
2023-06-17 07:57:12 +0300

    edited Navbar

--- frontend/src/pages/Navbar.jsx
+++ frontend/src/pages/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Navbar = () => {
-  const isLogin = true;
+  const isLogin = false;
   return (
     <nav className="bg-[#E2E2E2] shadow-md fixed w-full z-50  ">
       <div className="max-w-screen-xl md:mx-20 flex flex-wrap items-center justify-between  p-2">
@@ -65,7 +65,7 @@ const Navbar = () => {
           
               <li>
                 <a
-                  href="#"
+                  href="/home"
                   className="block btn py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 >
                   <button type="button" className="py-1.5 my-2 px-5 mr-2  text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
@@ -88,7 +88,9 @@ const Navbar = () => {
                   href="#"
                   className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 >
-                 <div className="h-[7vh] w-[7vh] rounded-full bg-gray-900" ></div>
+                  <div className="h-[7vh] w-[7vh] rounded-full bg-gray" >
+                    <img src ={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQduQaw52XvsA9qaIF5DwgFcM-18n8ilqYBiTkXn5a_yWKrrxWk"} className=" rounded-full" />
+                 </div>
                 </a>
               </li>
           
