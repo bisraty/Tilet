afed117362da07ebd61315e24a855f0b710bf9c9
bisraty <bisrat2007manu.com>
2023-06-20 17:09:40 +0300

    created profile page

---
+++ frontend/src/Profile/Profile.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../pages/Navbar'
+
+function Profile() {
+  return (
+      <div>
+          <Navbar />
+          <div>
+              <div className='bg-gray h-[10vh]'></div>
+          </div>
+    </div>
+  )
+}
+
+export default Profile
