4ff007b76d77ed5ebf4dc385b2b318d2ef134ea3
bisraty <bisrat2007manu.com>
2023-06-29 17:23:57 +0300

    added new package

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    // 'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+  ],
   theme: {
     extend: {
       colors: {
-        tertiary: "#535257",
+        tertiary: '#535257',
       },
     },
   },
   plugins: [],
-};
+}
