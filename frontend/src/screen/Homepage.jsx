e627b87388dd16c4974d229f4e083bf61bc5f172
bisraty <bisrat2007manu.com>
2023-06-29 17:28:30 +0300

    edited  homepage

--- frontend/src/screen/Homepage.jsx
+++ frontend/src/screen/Homepage.jsx
@@ -1,26 +1,25 @@
-import React, { useEffect } from 'react'
+import React, {useEffect} from 'react'
 import Home from '../pages/Home'
 import CanvasModel from '../canvas'
 import state from '../store'
 import Searchpage from '../pages/Searchpage'
 import Navbar from '../pages/Navbar'
+import Footer from '../pages/Footer'
 
 export default function Homepage() {
- 
-    
-    return (
+  return (
+    <div>
       <div>
-        <div >
-          <Navbar />
-          <br/>
-        <main className="app bg-[#E2E2E2]  ">
+        <Navbar />
+        <br />
+        <main className='app bg-[#E2E2E2]  '>
           <Home />
-        <CanvasModel />     
-            </main>
-        </div>
-       
-            <Searchpage />
+          <CanvasModel />
+        </main>
       </div>
-   
+
+      <Searchpage />
+      <Footer />
+    </div>
   )
 }
