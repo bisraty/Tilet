6a9bcf393cb18e809f18675c226132c98cc1365e
bisraty <bisrat2007manu.com>
2023-07-04 10:21:04 +0300

    fec: made routes secured

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,23 @@ import Createpage from './screen/Createpage'
 import Homepage from './screen/Homepage'
 import {Route, Routes} from 'react-router-dom'
 import ScrollToTop from './utils/ScrollToTop'
+import {useAuth} from './utils/auth-hook'
 
 function App() {
+  const {token} = useAuth()
   return (
     <div>
       <ScrollToTop />
       <Routes>
         <Route path='/' element={<Homepage />} />
         <Route path='/home' element={<Homepage />} />
-        <Route path='/create' element={<Createpage />} />
         <Route path='/detail' element={<Detailpage />} />
         <Route path='/login' element={<Loginpage />} />
         <Route path='/signup' element={<Signuppage />} />
-        <Route path='/profile' element={<Profile />} />
-        <Route path='/upload' element={<UploadCreatedFile />} />
+
+        <Route path='/create' element={!token ? <Homepage /> : <Createpage />} />
+        <Route path='/profile' element={!token ? <Homepage /> : <Profile />} />
+        <Route path='/upload' element={!token ? <Homepage /> : <UploadCreatedFile />} />
 
         <Route path='*' element={<Homepage />} />
       </Routes>
